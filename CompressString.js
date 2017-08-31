de55eaf6f752a6404e1471916d07926562ba23c8
Ray McCann <rmcc3@users.noreply.github.com>
2017-08-30 20:49:17 -0400

    Update CompressString.js

--- CompressString.js
+++ CompressString.js
@@ -2,6 +2,7 @@
 // Output: a3b2c1d4a1
 
 class CompressString {
+    // Create the constructor and require a string.
     constructor(string) {
         if (typeof string !== 'string')
             return new TypeError('Input must be a string');
@@ -10,14 +11,16 @@ class CompressString {
         this.builtString = '';
     }
 
+    // Loop through the string to find duplicates, and then compress it.
     findDuplicates() {
         if (this.str.length <= 0)
             throw new TypeError('Input string is empty');
 
+        // The count for the duplicates should always start at 1.
         let count = 1;
         for (let i = 0; i < this.str.length; i++) {
-            let current = this.str.substr(i, 1);
-            let next = this.str.substr(i+1, 1);
+            let current = this.str.substr(i, 1); // The current letter.
+            let next = this.str.substr(i+1, 1); // The next letter to compare to.
 
             if (current === next) {
                 count++;
@@ -32,3 +35,6 @@ class CompressString {
 
 const meow = new CompressString('aaabbcdddda');
 meow.findDuplicates();
+
+const meow2 = new CompressString('abcdefg');
+meow2.findDuplicates();
