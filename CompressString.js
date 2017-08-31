ee25631ba1e1bd681016e23d274c55af703a2a4e
Ray McCann <rmcc3@users.noreply.github.com>
2017-08-31 09:20:30 -0400

    Update CompressString.js

--- CompressString.js
+++ CompressString.js
@@ -29,7 +29,7 @@ class CompressString {
                 count = 1;
             }
         }
-        return this.builtString;
+        return ((this.builtString !== '') ? this.builtString : 'Empty');
     }
 }
 
